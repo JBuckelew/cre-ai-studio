3b92f40956df9c9c033a441685651666dd2886bb
Jonathan Buckelew <jonathan.buckelew@gmail.com>
2026-01-21 01:51:45 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-slate-50">
       <HeroSection />
-      <HowItWorksSection />
       <ValuePropositionSection />
+      <HowItWorksSection />
       <ToolsSection />
       
       {/* Section Break */}

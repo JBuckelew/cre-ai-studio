c5ce4d91618b919b9d037351abb373f0488792bd
Jonathan Buckelew <jonathan.buckelew@gmail.com>
2026-01-21 15:20:14 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,9 @@ export default function Home() {
       <HeroSection />
       <WhyChooseSection />
       <MissionSection />
+      <TestimonialsSection />
       <ValuePropositionSection />
       <HowItWorksSection />
-      <TestimonialsSection />
       <ToolsSection />
 
       {/* Section Break */}
